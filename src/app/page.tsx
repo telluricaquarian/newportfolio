dc85c1c1f5d67a3c9d014eab85afacb8d8dd00c5
telluricaquarian <llewellyn@telluricaquarian.com>
2025-02-16 21:36:46 +1100

    Updated portfolio content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,12 +120,12 @@ export default function Page() {
                   My Projects
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
+                  Check out my work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                  websites to simple web applications. Here are a few of my
+                  visual assets & projects.
                 </p>
               </div>
             </div>
