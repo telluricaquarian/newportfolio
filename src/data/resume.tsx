a11e29230e544b8b2f3857c491bbcbea219c397f
telluricaquarian <llewellyn@telluricaquarian.com>
2025-02-17 03:10:34 +1100

    Updated portfolio content

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -233,7 +233,7 @@ export const DATA = {
       ],
       image: "",
       video:
-        "https://telluricaquarian.com/wp-content/uploads/2025/02/Thumbnail-7.mp4",
-      },
+      "https://telluricaquarian.com/wp-content/uploads/2025/02/Thumbnail-7.mp4",
+    },
     ],
-  } as const;
+    } as const;
