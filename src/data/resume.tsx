4405694f49da702a207b88217138d30853f4e501
telluricaquarian <llewellyn@telluricaquarian.com>
2025-02-16 20:47:46 +1100

    Updated portfolio content

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -193,7 +193,7 @@ export const DATA = {
       ],
       image: "",
       video:
-        "<iframe width="560" height="315" src="https://www.youtube.com/embed/4umTJOJ1ydI?si=oCI-ge1smIbP-hSc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>",
+        "https://telluricaquarian.com/wp-content/uploads/2025/02/Thumbnail-3.mp4",
     },
     {
       title: "Magic UI",
